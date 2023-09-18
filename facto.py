fd268244a187095c0a1d14370fae2fb6a02b01ec
almacode1703 <testmygitt.2023@gmail.com>
2023-09-18 11:49:24 +0530

    files submitting...

---
+++ facto.py
@@ -0,0 +1,22 @@
+def factorial(n):
+  """Calculates the factorial of a number.
+
+  Args:
+    n: The number to calculate the factorial of.
+
+  Returns:
+    The factorial of n.
+  """
+
+  if n < 0:
+    raise ValueError("Factorial is not defined for negative numbers.")
+  elif n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+# Example usage:
+
+print(factorial(5))  # 120
+print(factorial(10))  # 3628800
